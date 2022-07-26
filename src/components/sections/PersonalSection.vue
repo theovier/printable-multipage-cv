944ad54f4a8b944f9c3fa1a411cc0edd18a4719d
theovier <theo@harkenbusch.com>
2022-07-26 11:12:31 +0200

    add email to personal data

--- src/components/sections/PersonalSection.vue
+++ src/components/sections/PersonalSection.vue
@@ -4,6 +4,7 @@
 		<div class="space-y-2">
 			<entry cue="Name">John Doe</entry>
 			<entry cue="Date of Birth">27.04.1997, Germany</entry>
+			<entry cue="Email">John@Example.com</entry>
 		</div>
 	</div>
 </template>
