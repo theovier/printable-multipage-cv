83bffecf9742929165ad4ba8c5076506f10efe3a
theovier <theo@harkenbusch.com>
2022-07-28 10:15:29 +0200

    add env for personal section

--- src/components/sections/PersonalSection.vue
+++ src/components/sections/PersonalSection.vue
@@ -2,9 +2,9 @@
 	<div>
 		<section-header text="Personal Data" />
 		<div class="space-y-2">
-			<entry cue="Name">John Doe</entry>
-			<entry cue="Date of Birth">27.04.1997, Germany</entry>
-			<entry cue="Email">John@Example.com</entry>
+			<entry cue="Name">{{ name }}</entry>
+			<entry cue="Date of Birth">{{ birthday }}, {{ bornIn }}</entry>
+			<entry cue="Email">{{ email }}</entry>
 		</div>
 	</div>
 </template>
@@ -12,4 +12,10 @@
 <script setup>
 import SectionHeader from "@/components/headers/SectionHeader.vue";
 import Entry from "@/components/common/Entry.vue";
+
+const name = import.meta.env.VITE_FULL_NAME;
+const email = import.meta.env.VITE_EMAIL;
+const birthday = import.meta.env.VITE_BIRTHDAY;
+const bornIn = import.meta.env.VITE_BORN_IN;
+
 </script>
